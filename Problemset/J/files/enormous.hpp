52a2c9d6e54061a53e6b8af23102edd28c25c159
escapist <redefined@yeah.net>
2023-11-26 09:12:13 +0800

    new file:   Problemset/J/files/enormous.hpp 	deleted:    Problemset/testlib.h

---
+++ Problemset/J/files/enormous.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+template <typename T, size_t BitLen> class Enormous {
+	short sign;
+	size_t length;
+	std::vector<T> data;
+	friend Enormous &operator+=(Enormous x) {
+		for (size_t i = 0; i < length && i < x.length; ++i) {
+			data[i] += x.data[i];
+		}
+		T res = 0;
+		for (size_t i = 0; i < length; ++i) {
+			data[i] += res;
+			if (data[i] >= (1 << BitLen)) {
+				res = data[i] - (1 << BitLen) + 1;
+			}
+		}
+		if (res) data.push_back(res);
+	}
+};
