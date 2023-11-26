e01875fe95c14ec7ef09a7dac1cfc9f2939eea15
escapist <redefined@yeah.net>
2023-11-26 10:48:01 +0800

    Update enormous.hpp

--- Problemset/J/files/enormous.hpp
+++ Problemset/J/files/enormous.hpp
@@ -18,4 +18,18 @@ template <typename T, size_t BitLen> class Enormous {
 		}
 		if (res) data.push_back(res);
 	}
+
+	friend Enormous &operator-=(Enormous x) {
+		for (size_t i = 0; i < length && i < x.length; ++i) {
+			data[i] -= x.data[i];
+		}
+		T res = 0;
+		for (size_t i = 0; i < length; ++i) {
+			data[i] += res;
+			if (data[i] >= (1 << BitLen)) {
+				res = data[i] - (1 << BitLen) + 1;
+			}
+		}
+		if (res) data.push_back(res);
+	}
 };
