002d6e9f9a8c67793e06808cd9fc87d061f3275b
escapist <redefined@yeah.net>
2023-11-29 19:38:39 +0800

    update contest.pdf

--- Statement/run.bat
+++ Statement/run.bat
@@ -1,1 +1,2 @@
 .\render\cpdf.exe -bottom 40 -font "Courier-Bold" -add-text "-- %%Page / %%EndPage --" .\statement.pdf -o .\statement.pdf
+.\render\cpdf.exe -merge .\head.pdf .\info.pdf .\statement.pdf -o .\contest.pdf
