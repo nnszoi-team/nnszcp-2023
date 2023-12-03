1c9d23f795c0240741583eda5cb28cddd528d34b
c1ampy <147057564+c1ampy@users.noreply.github.com>
2023-12-03 09:37:45 +0000

    fraction.hpp modified

--- Problemset/J/files/fraction.hpp
+++ Problemset/J/files/fraction.hpp
@@ -12,8 +12,13 @@ template <typename T> std::string to_string(T data) {
 	return cur;
 }
 
-template <typename T> T gcd(const T a, const T b) { return b ? gcd(b, a % b) : a; }
-template <typename T> T lcm(const T a, const T b) { return a / gcd(a, b) * b; }
+template <typename T> T gcd(const T a, const T b) {
+	return b ? gcd(b, a % b) : a;
+}
+template <typename T> T lcm(const T a, const T b) {
+	return a / gcd(a, b) * b;
+}
+
 template <typename T> class Fraction {
   protected:
 	void simplify() {
@@ -34,57 +39,60 @@ template <typename T> class Fraction {
 
 		T g = gcd(this->up, this->down);
 		this->up /= g, this->down /= g;
-		this->real = (double)this->up / this->down * sign;
+		this->real = (double)this->up / this->down * this->sign;
 	}
 
   public:
 	int sign;
 	T up, down;
 	double real;
-	Fraction() : up(0), down(1), real(0) { simplify(); }
+
+	Fraction() : up(0), down(1), real(0) {
+		simplify();
+	}
 	Fraction(const T up, const T down) : up(up), down(down), real((double)up / down) {
 		simplify();
 	}
 
-	std::string data() {
-		std::string _sign;
-		if (sign == 1)
-			_sign += "+";
-		else if (sign == -1)
-			_sign += "-";
-		auto _up = to_string(up);
-		_up += "/";
-		auto _down = to_string(down);
-		_sign += _up;
-		_sign += _down;
-		return _sign;
+	std::string data() const {
+		std::string _sign = this->sign == 1 ? "+" : this->sign == -1 ? "-" : "";		
+		std::string _up = to_string(this->up);
+		std::string _down = to_string(this->down);
+		return _sign + _up + "/" + _down;
 	}
 
-	double value() const { return this->real; }
-
-	friend std::ostream &operator<<(std::ostream &os, Fraction f) {
+	double value() const {
+		return this->real;
+	}
+	friend std::ostream &operator<<(std::ostream &os, const Fraction &f) {
 		os << f.data();
 		return os;
 	}
-
-	friend bool operator<(Fraction a, Fraction b) {
+	friend bool operator<(const Fraction &a, const Fraction &b) {
 		return a.sign * a.up * b.down < b.sign * b.up * a.down;
 	}
-	friend bool operator>(Fraction a, Fraction b) {
+	friend bool operator>(const Fraction &a, const Fraction &b) {
 		return a.up * b.down > b.up * a.down;
 	}
-	friend bool operator==(Fraction a, Fraction b) { return !(a < b || a > b); }
-	friend bool operator<=(Fraction a, Fraction b) { return a < b || a == b; }
-	friend bool operator>=(Fraction a, Fraction b) { return a > b || a == b; }
-	friend bool operator!=(Fraction a, Fraction b) { return !(a == b); }
+	friend bool operator==(const Fraction &a, const Fraction &b) {
+		return !(a < b || a > b);
+	}
+	friend bool operator<=(const Fraction &a, const Fraction &b) {
+		return a < b || a == b;
+	}
+	friend bool operator>=(const Fraction &a, const Fraction &b) {
+		return a > b || a == b;
+	}
+	friend bool operator!=(const Fraction &a, const Fraction &b) {
+		return !(a == b);
+	}
 	friend Fraction operator+(const Fraction &a, const Fraction &b) {
 		T l = lcm(a.down, b.down);
 		Fraction res(a.up * (l / a.down) * a.sign + b.up * (l / b.down) * b.sign, l);
 		return res;
 	}
 	Fraction &operator+=(const Fraction &other) {
-		*this = *this + other;
-		return *this;
+		return *this = *this + other;
 	}
 	friend Fraction operator-(const Fraction &a, const Fraction &b) {
 		T l = lcm(a.down, b.down);
@@ -92,23 +100,24 @@ template <typename T> class Fraction {
 		return res;
 	}
 	Fraction &operator-=(const Fraction &other) {
-		*this = *this - other;
-		return *this;
+		return *this = *this - other;
 	}
 	friend Fraction operator*(const Fraction &a, const Fraction &b) {
 		return Fraction(a.up * b.up * a.sign * b.sign, a.down * b.down);
 	}
 	Fraction &operator*=(const Fraction &other) {
-		*this = (*this) * other;
-		return *this;
+		return *this = (*this) * other;
 	}
-	Fraction inv() const { return Fraction(this->down * this->sign, this->up); }
-	friend Fraction inv(const Fraction &f) { return f.inv(); }
+	Fraction inv() const {
+		return Fraction(this->down * this->sign, this->up);
+	}
+	friend Fraction inv(const Fraction &f) {
+		return f.inv();
+	}
 	friend Fraction operator/(const Fraction &a, const Fraction &b) {
 		return a * b.inv();
 	}
 	Fraction &operator/=(const Fraction &other) {
-		*this = (*this) / other;
-		return *this;
+		return *this = (*this) / other;
 	}
 };
