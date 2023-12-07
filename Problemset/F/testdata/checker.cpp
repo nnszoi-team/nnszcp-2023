35c4fdf8c96a3cbedd319b0633df69de60938ba6
escapist404 <xiong@23118>
2023-12-07 18:37:57 +0800

    fix sample of problem A

--- Problemset/F/testdata/checker.cpp
+++ Problemset/F/testdata/checker.cpp
@@ -2,25 +2,23 @@
 
 const double eps = 1e-5;
 
-int main(int argc, char* argv[]) {
-  registerTestlibCmd(argc, argv);
+int main(int argc, char *argv[]) {
+	registerTestlibCmd(argc, argv);
 
-  int T = inf.readInt();
+	int T = inf.readInt();
 
-  while (T--) {
-    double ans_1 = ans.readDouble();
-    int ans_2 = ans.readInt();
+	for (int i = 1; i <= T; ++i) {
+		long double answer = ans.readDouble();
+		long double out = ouf.readDouble();
 
-    double out = ouf.readDouble();
+		long double delta = fabs(out - answer);
 
-    double delta_1 = fabs(out - ans_1);
-    double delta_2 = fabs(out - ans_2);
+		if (delta > eps && delta / answer > eps) {
+			quitf(_wa,
+				  "Testcase #%d: the answer is wrong: expected %Lf, found %Lf.",
+				  i, answer, out);
+		}
+	}
 
-    if (delta_1 > eps && delta_1 / ans_1 > eps && delta_2 > eps) {
-      quitf(_wa, "The answer is wrong: expected %f or %f, found %f.", ans_1,
-            ans_2, out);
-    }
-  }
-
-  quitf(_ok, "The answer is correct.");
+	quitf(_ok, "The answer is correct.");
 }
